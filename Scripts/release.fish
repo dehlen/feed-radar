4e31b4e3316b8bfa1dcb95daa221a50045e591f6
Levitating Pineapple <noreply@levitatingpineapple.com>
2025-01-05 00:50:26 +0200

    minor

--- Scripts/release.fish
+++ Scripts/release.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+function space_to_continue
+    read input \
+        --prompt "echo \n(set_color magenta)Press space to continue!\n" \
+        --silent \
+        --nchars 1
+    if test $input != " "
+        exit 1
+    end
+end
+
 set new_version $argv[1]
 set archive_path FeedRadar.xcarchive
 
@@ -8,38 +18,37 @@ agvtool next-version
 
 # Set marketing version 
 # Using `sed` since `agvtool new-marketing-version` seems to be broken
-sed -E \
+sed -E -i "" \
     "s/MARKETING_VERSION = [0-9.]+;/MARKETING_VERSION = $new_version;/g" \
     FeedRadar.xcodeproj/project.pbxproj
 
 # Update tag and push changes
 git diff
-read input \
-    --prompt "echo \n(set_color magenta)Press space to continue!\n" \
-    --silent \
-    --nchars 1
-if test $input != " "
-    exit 1
-end
-# git add .
-# git commit -m "release $new_version"
-# git push
-# git tag $new_version
-# git push --tags
+space_to_continue
+git add .
+git commit -m "release $new_version"
+git push
+git tag $new_version
+git push --tags
 
+# Build archive
+xcodebuild \
+    -project FeedRadar.xcodeproj \
+    -scheme FeedRadar \
+    -sdk iphoneos \
+    -configuration Release \
+    archive -archivePath archive_path |
+    xcbeautify
 
-# # Build archive
-# xcodebuild \
-#     -project FeedRadar.xcodeproj \
-#     -scheme FeedRadar \
-#     -sdk iphoneos \
-#     -configuration Release \
-#     archive -archivePath archive_path |
-#     xcbeautify
+# Upload to appstore connect
+xcodebuild \
+    -exportArchive \
+    -archivePath archive_path \
+    -exportOptionsPlist App/ExportOptions.plist \
+    -exportPath export
 
-# # Upload to appstore connect
-# xcodebuild \
-#     -exportArchive \
-#     -archivePath archive_path \
-#     -exportOptionsPlist App/ExportOptions.plist \
-#     -exportPath export
+# Open 
+echo "TestFlight -> Crypto compliance"
+echo "Extrnal Testers -> Add latest build"
+space_to_continue
+open https://appstoreconnect.apple.com/apps
